2836f92e923b52f487b69ccdf9fe24537bce5628
samuelwigren <samuel.wigren@gsfacket.se>
2022-12-03 13:56:18 +0000

    Add create ships function

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import random
+
+
 # Create empty lists for ships and hits
 Hidden_Pattern = [[' ']*8 for x in range(8)]
 
@@ -12,5 +15,17 @@ def print_board(board):
         print("%d| %s |" % (row_num, " | ".join(row)))
         row_num += 1
 
+
+def create_ships(board):
+    # Function to create ships
+    row_list = random.sample(range(0, 7), 5)
+    col_list = random.sample(range(0, 7), 5)        
+    for row, col in zip(row_list, col_list):
+        board[row][col] = 'X'
+
+
+create_ships(Hidden_Pattern)
+print(' Welcome to Battleship!')
+print()
 
 print_board(Hidden_Pattern)
