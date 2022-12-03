746b18041afa29b7ee014f39f3a880728cb3ea51
samuelwigren <samuel.wigren@gsfacket.se>
2022-12-03 14:48:42 +0000

    Add function to get player input

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import random
 
 # Create empty lists for ships and hits
 Hidden_Pattern = [[' ']*8 for x in range(8)]
+Guess_Pattern = [[' ']*8 for x in range(8)]
+
+col_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
 def print_board(board):
@@ -24,8 +27,27 @@ def create_ships(board):
         board[row][col] = 'X'
 
 
+def get_player_guess():
+    # Enter the row number between 1 to 8
+    print()
+    row = input('Please enter a number between 1 and 8: \n')
+    while not row or row not in '12345678':
+        print("Please enter a valid number ")
+        row = input('Please enter a number between 1 and 8: \n')
+    # Enter the Ship column from A TO H
+    col = input('Please enter a letter between A and H: \n').upper()
+    while not col or col not in 'ABCDEFGH':
+        print("Please enter a valid letter ")
+        col = input('Please enter a letter between A and H: \n').upper()
+
+    print()
+    return int(row)-1, col_num[col]
+
+
 create_ships(Hidden_Pattern)
 print(' Welcome to Battleship!')
 print()
 
 print_board(Hidden_Pattern)
+print_board(Guess_Pattern)
+row, col = get_player_guess()
