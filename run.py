9767aab6742a08eff05a8ae4c6229d3a150d49fd
samuelwigren <samuel.wigren@gsfacket.se>
2022-12-03 11:37:13 +0000

    Add first funkton to print board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Create empty lists for ships and hits
+Hidden_Pattern = [[' ']*8 for x in range(8)]
+
+
+def print_board(board):
+    # Create board for the game
+    print()
+    print('   A   B   C   D   E   F   G   H')
+    print('  *** *** *** *** *** *** *** ***')
+    row_num = 1
+    for row in board:
+        print("%d| %s |" % (row_num, " | ".join(row)))
+        row_num += 1
+
+print_board(Hidden_Pattern)
