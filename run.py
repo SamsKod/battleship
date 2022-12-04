e0c29733ee38cd34842787e48b226c1b8b303231
samuelwigren <samuel.wigren@gsfacket.se>
2022-12-04 11:49:40 +0000

    Change  game over text to player

--- run.py
+++ run.py
@@ -99,6 +99,6 @@ while turns > 0:
         print('      GAME OVER!')
         print()
         print(' Here is your guesses and hits.')
-        print(' You see the hidden ships aswell (X)')
+        print(' You see the hidden ships (X)')
         print_final_board(Guess_Pattern, Hidden_Pattern)
         break
